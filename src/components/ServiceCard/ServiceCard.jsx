896c7d37904b235fd2c829840b0ee68274d56b35
Bosui <rbutauskas@gmail.com>
2024-10-29 19:08:52 +0200

    po uzluzimo perdarytas naujai

--- src/components/ServiceCard/ServiceCard.jsx
+++ src/components/ServiceCard/ServiceCard.jsx
@@ -3,14 +3,11 @@
 import React from 'react';
 import styles from './ServiceCard.module.scss';
 
-const ServiceCard = ({ title, icon, contactPerson = "Tadas Gedvilas", address = "Kauno g. 27, Vilnius" }) => {
+const ServiceCard = ({ title, icon }) => {
   return (
     <div className={styles.card}>
-      {icon && <div className={styles.icon}>{icon}</div>}
-      <div className={styles.infoContainer}>
-        <span className={styles.chip}>cleaning</span>
-        
-      </div>
+      <div className={styles.icon}>{icon}</div>
+      <span className={styles.textLabel}>{title}</span>
     </div>
   );
 };
